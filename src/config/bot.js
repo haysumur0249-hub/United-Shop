b2849bde150300dc2e1367d6babd922f9bb5e41b
MrZooma <haysumur0249@gmail.com>
2026-04-04 14:02:08 +0600

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️, ☣️ To Proteced The server Trusted ,💥 Fast service",
+        name: "Made For Service",
         // Activity type number (0 = Playing).
         type: 0, 
       },
